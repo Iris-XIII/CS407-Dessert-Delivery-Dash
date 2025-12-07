d177e674743262beb33f99d3b0630b9f2f64df0b
Maria Shackett <mjs062004@gmail.com>
2025-12-06 18:07:17 -0600

    Fixed issues in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,15 @@ import 'screens/kitchen_screen.dart';
 import 'screens/customer_reception_screen.dart';
 import 'screens/recipe_screen.dart';
 import 'screens/settings_screen.dart';
+// Firebase imports
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(DessertDeliveryDash());
 }
 
