b29946922aa2e23e50074cf10a9fdbb88ffe8086
benstroeher <ben.stuart.stroeher@gmail.com>
2025-12-09 00:41:47 -0600

    Complete Game Flow

--- lib/data/days.dart
+++ lib/data/days.dart
@@ -1,48 +1,93 @@
-import 'package:dessert_delivery_dash/models/day_plan.dart';
-
 import '../models/day_plan.dart';
 
 /// 30 days, max 8 customers per day, roughly increasing difficulty.
+///
+/// Valid character IDs (from characters.dart):
+///   Bear, Deer, Fairy, Female Elf, Fox, Frog,
+///   Gnome, Hedgehog, Owl, Panda, Raccoon, Squirrel, Wizard
 const List<day_plan> kDays = [
   // Days 1–5: very easy, 2–3 customers
-  day_plan(dayNumber: 1, characterIds: ["Owl", "bear"]),
+  day_plan(dayNumber: 1, characterIds: ["Owl", "Bear"]),
   day_plan(dayNumber: 2, characterIds: ["Fox", "Owl", "Frog"]),
-  day_plan(dayNumber: 3, characterIds: ["Frog", "cat", "squirrel"]),
-  day_plan(dayNumber: 4, characterIds: ["frog", "mouse", "owl"]),
-  day_plan(dayNumber: 5, characterIds: ["rabbit", "hedgehog", "deer"]),
+  day_plan(dayNumber: 3, characterIds: ["Frog", "Squirel", "Bear"]),
+  day_plan(dayNumber: 4, characterIds: ["Owl", "Panda", "Deer"]),
+  day_plan(dayNumber: 5, characterIds: ["Hedgehog", "Squirel", "Fox"]),
 
   // Days 6–10: 3–4 customers, mix of tea + cake
-  day_plan(dayNumber: 6, characterIds: ["owl", "bear", "squirrel", "cat"]),
-  day_plan(dayNumber: 7, characterIds: ["fox", "dog", "frog", "mouse"]),
-  day_plan(dayNumber: 8, characterIds: ["deer", "panda", "rabbit", "bear"]),
-  day_plan(dayNumber: 9, characterIds: ["raccoon", "cat", "owl", "hedgehog"]),
-  day_plan(dayNumber: 10, characterIds: ["squirrel", "koala", "fox", "mouse"]),
+  day_plan(dayNumber: 6, characterIds: ["Owl", "Bear", "Squirel", "Fox"]),
+  day_plan(dayNumber: 7, characterIds: ["Fox", "Frog", "Panda", "Deer"]),
+  day_plan(dayNumber: 8, characterIds: ["Hedgehog", "Raccoon", "Owl", "Bear"]),
+  day_plan(dayNumber: 9, characterIds: ["Squirel", "Panda", "Frog", "Deer"]),
+  day_plan(dayNumber: 10, characterIds: ["Raccoon", "Fox", "Hedgehog", "Owl"]),
 
-  // Days 11–15: 4–5 customers
-  day_plan(dayNumber: 11, characterIds: ["owl", "fox", "bear", "cat", "frog"]),
-  day_plan(dayNumber: 12, characterIds: ["rabbit", "dog", "deer", "mouse", "panda"]),
-  day_plan(dayNumber: 13, characterIds: ["raccoon", "hedgehog", "squirrel", "cat", "owl"]),
-  day_plan(dayNumber: 14, characterIds: ["frog", "bear", "koala", "fox", "mouse"]),
-  day_plan(dayNumber: 15, characterIds: ["panda", "rabbit", "deer", "cat", "hedgehog"]),
+  // Days 11–15: 4–5 customers, start introducing magic characters
+  day_plan(dayNumber: 11, characterIds: ["Fairy", "Owl", "Fox", "Bear"]),
+  day_plan(dayNumber: 12, characterIds: ["Frog", "Squirel", "Panda", "Deer"]),
+  day_plan(dayNumber: 13, characterIds: ["Hedgehog", "Raccoon", "Fairy", "Owl", "Fox"]),
+  day_plan(dayNumber: 14, characterIds: ["Bear", "Squirel", "Panda", "Frog", "Deer"]),
+  day_plan(dayNumber: 15, characterIds: ["Fox", "Hedgehog", "Raccoon", "Owl", "Fairy"]),
 
-  // Days 16–20: 5–6 customers
-  day_plan(dayNumber: 16, characterIds: ["owl", "fox", "squirrel", "bear", "cat", "mouse"]),
-  day_plan(dayNumber: 17, characterIds: ["frog", "raccoon", "deer", "dog", "hedgehog", "rabbit"]),
-  day_plan(dayNumber: 18, characterIds: ["panda", "koala", "owl", "cat", "mouse", "bear"]),
-  day_plan(dayNumber: 19, characterIds: ["fox", "squirrel", "frog", "panda", "dog", "hedgehog"]),
-  day_plan(dayNumber: 20, characterIds: ["rabbit", "deer", "raccoon", "cat", "mouse", "koala"]),
+  // Days 16–20: 5–6 customers, add Gnome and Wizard
+  day_plan(dayNumber: 16, characterIds: ["Gnome", "Owl", "Bear", "Squirel", "Fox"]),
+  day_plan(dayNumber: 17, characterIds: ["Frog", "Panda", "Deer", "Hedgehog", "Raccoon"]),
+  day_plan(dayNumber: 18, characterIds: ["Fairy", "Gnome", "Owl", "Fox", "Squirel", "Bear"]),
+  day_plan(dayNumber: 19, characterIds: ["Panda", "Frog", "Deer", "Hedgehog", "Raccoon", "Owl"]),
+  day_plan(dayNumber: 20, characterIds: ["Wizard", "Fox", "Squirel", "Bear", "Gnome", "Fairy"]),
 
-  // Days 21–25: 6–7 customers
-  day_plan(dayNumber: 21, characterIds: ["owl", "fox", "squirrel", "bear", "cat", "frog", "mouse"]),
-  day_plan(dayNumber: 22, characterIds: ["rabbit", "deer", "panda", "dog", "hedgehog", "koala", "owl"]),
-  day_plan(dayNumber: 23, characterIds: ["raccoon", "fox", "squirrel", "cat", "mouse", "bear", "frog"]),
-  day_plan(dayNumber: 24, characterIds: ["deer", "rabbit", "panda", "koala", "hedgehog", "dog", "owl"]),
-  day_plan(dayNumber: 25, characterIds: ["fox", "squirrel", "frog", "bear", "cat", "mouse", "raccoon"]),
+  // Days 21–25: 6–7 customers, include Female Elf
+  day_plan(dayNumber: 21, characterIds: ["Owl", "Fox", "Squirel", "Bear", "Frog", "Panda", "Deer"]),
+  day_plan(dayNumber: 22, characterIds: ["Hedgehog", "Raccoon", "Gnome", "Fairy", "Female Elf", "Owl"]),
+  day_plan(dayNumber: 23, characterIds: ["Fox", "Squirel", "Bear", "Frog", "Panda", "Deer", "Female Elf"]),
+  day_plan(dayNumber: 24, characterIds: ["Raccoon", "Hedgehog", "Gnome", "Fairy", "Wizard", "Owl", "Fox"]),
+  day_plan(dayNumber: 25, characterIds: ["Bear", "Squirel", "Panda", "Frog", "Deer", "Female Elf", "Wizard"]),
 
   // Days 26–30: 7–8 customers (max difficulty)
-  day_plan(dayNumber: 26, characterIds: ["owl", "fox", "squirrel", "bear", "cat", "frog", "mouse", "panda"]),
-  day_plan(dayNumber: 27, characterIds: ["rabbit", "deer", "raccoon", "dog", "hedgehog", "koala", "owl", "cat"]),
-  day_plan(dayNumber: 28, characterIds: ["panda", "fox", "squirrel", "bear", "mouse", "frog", "deer", "rabbit"]),
-  day_plan(dayNumber: 29, characterIds: ["raccoon", "koala", "hedgehog", "cat", "dog", "owl", "squirrel", "mouse"]),
-  day_plan(dayNumber: 30, characterIds: ["owl", "fox", "frog", "panda", "bear", "cat", "rabbit", "raccoon"]),
+  day_plan(dayNumber: 26, characterIds: [
+    "Owl",
+    "Fox",
+    "Squirel",
+    "Bear",
+    "Frog",
+    "Panda",
+    "Raccoon",
+  ]),
+  day_plan(dayNumber: 27, characterIds: [
+    "Hedgehog",
+    "Gnome",
+    "Fairy",
+    "Female Elf",
+    "Owl",
+    "Fox",
+    "Squirel",
+  ]),
+  day_plan(dayNumber: 28, characterIds: [
+    "Bear",
+    "Frog",
+    "Panda",
+    "Deer",
+    "Raccoon",
+    "Hedgehog",
+    "Gnome",
+    "Fairy",
+  ]),
+  day_plan(dayNumber: 29, characterIds: [
+    "Wizard",
+    "Owl",
+    "Fox",
+    "Squirel",
+    "Bear",
+    "Panda",
+    "Raccoon",
+    "Female Elf",
+  ]),
+  day_plan(dayNumber: 30, characterIds: [
+    "Wizard",
+    "Fairy",
+    "Female Elf",
+    "Gnome",
+    "Owl",
+    "Fox",
+    "Squirel",
+    "Bear",
+  ]),
 ];
